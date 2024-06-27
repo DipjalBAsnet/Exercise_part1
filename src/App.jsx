88ab71431db67ef7220907e489f54c7771ea7050
unknown <dipjal5678@gmai.com>
2024-06-27 20:30:54 +0545

    modified code as per 1.3, .4, .5

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,29 @@ import Content from "./component/content";
 import Total from "./component/total";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
-
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  const course = {
+    name: "Half stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total total={total} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
