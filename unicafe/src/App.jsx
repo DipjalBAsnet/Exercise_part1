ebfe73df24a72072ec54f816b34474ab38cd1c0f
unknown <dipjal5678@gmai.com>
2024-07-02 20:00:05 +0545

    1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,11 +1,41 @@
 import { useState } from "react";
 
 const App = () => {
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const total = good + neutral + bad;
+  const average = good + bad + neutral / 3;
+  const positive = (good / total) * 100;
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleBadClick}>bad</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <br />
+      <h1>Statistics</h1> <br />
+      <p>Good :{good}</p>
+      <p>Neutral :{neutral}</p>
+      <p>bad : {bad}</p>
+      <p>all : {total}</p>
+      <p>average : {average}</p>
+      <p>positive : {positive}%</p>
+    </div>
+  );
 };
 
 export default App;
